b43b08be9cf272e5250d0ab1beafeb0a29c7e78f
adrianPers <pers.adrian@outlook.com>
2025-05-14 10:21:35 -0300

    primeiro commit: upload de arquivos

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+const btnMenuHeader = document.getElementById('btn-menu-header')
+const menuLateral = document.getElementById('menu-lateral')
+
+btnMenuHeader.addEventListener('click', ()=>{
+
+    let isAberto = menuLateral.classList.toggle('menu-lateral-aberto') 
+    btnMenuHeader.children[0].innerHTML = (
+        isAberto ? 'close' : 'menu'
+    )
+    
+})
